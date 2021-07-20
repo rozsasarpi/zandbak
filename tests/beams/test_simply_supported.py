167fa52108120b2fd65eaf6b9b8cc3d5cedbc44f
Arpad Rozsas <rozsasarpi@gmail.com>
2021-07-20 23:38:24 +0200

    test(beams): add test for clamped-clamped beam

--- tests/beams/test_simply_supported.py
+++ tests/beams/test_simply_supported.py
@@ -1,4 +1,9 @@
-from zandbak.beams.simply_supported import hinged_hinged_beam_under_point_force
+import numpy as np
+
+from zandbak.beams.simply_supported import (
+    clamped_clamped_beam_under_point_force,
+    hinged_hinged_beam_under_point_force,
+)
 
 
 def test_hinged_hinged_beam_under_point_force():
@@ -8,7 +13,7 @@ def test_hinged_hinged_beam_under_point_force():
     load_position_from_left_end = span_length / 2
     position_from_left_end = load_position_from_left_end
 
-    d_x_expected = load_intensity * span_length ** 3 / (48 * flexural_stiffness)
+    d_x_expected = 1 / 48 * load_intensity * span_length ** 3 / flexural_stiffness
     m_x_expected = load_intensity * position_from_left_end / 2
 
     d_x, _, m_x = hinged_hinged_beam_under_point_force(
@@ -19,5 +24,27 @@ def test_hinged_hinged_beam_under_point_force():
         position_from_left_end=position_from_left_end,
     )
 
-    assert d_x == d_x_expected
-    assert m_x == m_x_expected
+    np.testing.assert_almost_equal(d_x, d_x_expected)
+    np.testing.assert_almost_equal(m_x, m_x_expected)
+
+
+def test_clamped_clamped_beam_under_point_force():
+    span_length = 10
+    flexural_stiffness = 3
+    load_intensity = 2
+    load_position_from_left_end = span_length / 2
+    position_from_left_end = load_position_from_left_end
+
+    d_x_expected = 1 / 192 * load_intensity * span_length ** 3 / flexural_stiffness
+    m_x_expected = load_intensity * position_from_left_end / 4
+
+    d_x, _, m_x = clamped_clamped_beam_under_point_force(
+        span_length=span_length,
+        flexural_stiffness=flexural_stiffness,
+        load_intensity=load_intensity,
+        load_position_from_left_end=load_position_from_left_end,
+        position_from_left_end=position_from_left_end,
+    )
+
+    np.testing.assert_almost_equal(d_x, d_x_expected)
+    np.testing.assert_almost_equal(m_x, m_x_expected)
